850361137594e017cd7dc79854b27e968df9594a
Christian Hernandez <christian.hernandez@yahoo.com>
2020-08-11 18:47:44 -0700

    no longer need the workshop dir

--- .workshop/develop-settings.sh
+++
@@ -1,2 +0,0 @@
-AUTH_USERNAME=workshop
-AUTH_PASSWORD=workshop
--- .workshop/settings.sh
+++
@@ -1,7 +0,0 @@
-WORKSHOP_NAME=ocp-ocs-admins
-WORKSHOP_IMAGE=quay.io/openshiftroadshow/lab-ocp-ocs:411-test
-WORKSHOP_TITLE="OpenShift and Container Storage for Admins"
-RESOURCE_BUDGET=medium
-MAX_SESSION_AGE=3600
-IDLE_TIMEOUT=300
-SPAWNER_MODE=terminal-server

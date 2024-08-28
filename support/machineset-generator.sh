a6e4039f2ff0059818ed4fb05f4d90ac5740fcb1
Josh Newsome <50967636+jnewsome97@users.noreply.github.com>
2024-08-28 10:45:14 +0100

    Update machineset-generator.sh test

--- support/machineset-generator.sh
+++ support/machineset-generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export AMI=$(oc get machineset -n openshift-machine-api -l 'machine.openshift.io/os-id!=Windows' -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.ami.id}')
-export CLUSTERID=$(oc get infrastructures.config.openshift.io cluster -o jsonpath='{.status.infrastructureName}')
-export REGION=$(oc get infrastructures.config.openshift.io cluster -o jsonpath='{.status.platformStatus.aws.region}')
+export CLUSTERID=$(oc get infrastructure cluster -o=jsonpath='{.status.infrastructureName}')
+export REGION=$(oc get infrastructure cluster -o=jsonpath='{.status.platformStatus.aws.region}')
 export COUNT=${1:-3}
 export NAME=${2:-workerocs}
 export SCALE=${3:-1}

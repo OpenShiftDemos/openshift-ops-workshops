4c5d745e45b9d464c9368166b93a7224c7bcc0ff
Josh Newsome <50967636+jnewsome97@users.noreply.github.com>
2024-08-29 15:19:01 +0100

    Update machineset-patch.sh
    issue with formatting (spaces)

--- support/machineset-patch.sh
+++ support/machineset-patch.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-export MACHINESET=$(oc get machineset.machine.openshift.io -n openshift-machine-api -l machine.openshift.io/cluste
-r-api-machine-role=infra -o jsonpath='{.items[0].metadata.name}')
-oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "add", "pa
-th": "/spec/template/spec/metadata/labels", "value":{"node-role.kubernetes.io/worker":"", "node-role.kubernetes.io
-/infra":""} }]'
+export MACHINESET=$(oc get machineset.machine.openshift.io -n openshift-machine-api -l machine.openshift.io/cluster-api-machine-role=infra -o jsonpath='{.items[0].metadata.name}')
+oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "add", "path": "/spec/template/spec/metadata/labels", "value":{"node-role.kubernetes.io/worker":"", "node-role.kubernetes.io/infra":""} }]'
 
-export WORKER_NAME=$(oc get machineset.machine.openshift.io -n openshift-machine-api | awk '/worker/{print $1}' |
-head -n 1)
-export SECURITY_GROUP=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.m
-etadata.name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.securityGroups[0].filters[0].values[0]}')
-oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace",
- "path": "/spec/template/spec/providerSpec/value/securityGroups/0/filters/0/values/0", "value": "'"$SECURITY_GROUP
-"'"}]'
+export WORKER_NAME=$(oc get machineset.machine.openshift.io -n openshift-machine-api | awk '/worker/{print $1}' | head -n 1)
+export SECURITY_GROUP=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.metadata.name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.securityGroups[0].filters[0].values[0]}')
+oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/providerSpec/value/securityGroups/0/filters/0/values/0", "value": "'"$SECURITY_GROUP"'"}]'
 
-export SUBNET=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.metadata.
-name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.subnet.filters[0].values[0]}')
-oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace",
- "path": "/spec/template/spec/providerSpec/value/subnet/filters/0/values/0", "value": "'"$SUBNET"'"}]'
+export SUBNET=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.metadata.name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.subnet.filters[0].values[0]}')
+oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/providerSpec/value/subnet/filters/0/values/0", "value": "'"$SUBNET"'"}]'
 
-export API_VERSION=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.meta
-data.name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.apiVersion}')
-oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace",
- "path": "/spec/template/spec/providerSpec/value/apiVersion", "value": "'"$API_VERSION"'"}]'
+export API_VERSION=$(oc get machineset.machine.openshift.io -n openshift-machine-api -o jsonpath='{.items[?(@.metadata.name=="'$WORKER_NAME'")].spec.template.spec.providerSpec.value.apiVersion}')
+oc patch machineset.machine.openshift.io $MACHINESET -n openshift-machine-api --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/providerSpec/value/apiVersion", "value": "'"$API_VERSION"'"}]'

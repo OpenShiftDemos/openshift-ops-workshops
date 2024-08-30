1da425cbde7b092775b784e9d71a3d1c3813cee1
Josh Newsome <50967636+jnewsome97@users.noreply.github.com>
2024-08-30 15:50:49 +0100

    Update machineset-generator.sh

--- support/machineset-generator.sh
+++ support/machineset-generator.sh
@@ -7,7 +7,3 @@ export NAME=${2:-workerocs}
 export SCALE=${3:-1}
 
 $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/machineset-cli -scale $SCALE -name $NAME -count $COUNT -ami $AMI -clusterID $CLUSTERID -region $REGION
-
-chmod +x /opt/app-root/src/support/machineset-patch.sh
-
-/opt/app-root/src/support/machineset-patch.sh

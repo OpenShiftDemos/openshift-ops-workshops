88acd01159d67ceacd9685895c9eba47b52d68ec
Josh Newsome <jnewsome@redhat.com>
2026-03-02 11:31:14 +0000

    MAD:OPS - Fix netproj pods crashing under restricted-v2 SCC
    - Replace DeploymentConfig with Deployment (DC deprecated in 4.14+)
    - Use ubi9/ubi with python3 http.server instead of the image-registry
      image which crashes under restricted-v2 trying to write to
      /etc/pki/ca-trust/
    - Remove IMAGE parameter since image is now hardcoded in template

--- support/create-net-projects.sh
+++ support/create-net-projects.sh
@@ -3,12 +3,8 @@
 # We want to bail if an error occurs.
 set -e
 
-
 cd $(dirname $0)
 
-
-image=$(oc get deployments -n openshift-image-registry image-registry --template '{{ range $x := .spec.template.spec.containers }} {{- $x.image -}} {{ end }}')
-
-# deploy the DC definition into the projects
-oc process -f netproj-template.yaml NAMESPACE=netproj-a IMAGE="$image" | oc apply -n netproj-a -f -
-oc process -f netproj-template.yaml NAMESPACE=netproj-b IMAGE="$image" | oc apply -n netproj-b -f -
+# deploy into the projects
+oc process -f netproj-template.yaml NAMESPACE=netproj-a | oc apply -n netproj-a -f -
+oc process -f netproj-template.yaml NAMESPACE=netproj-b | oc apply -n netproj-b -f -
